f5824366e1b2ad270c7b18a7782a232ad389b577
K.Fujiyoshi <259993369+i-cezuki@users.noreply.github.com>
2026-08-02 08:54:11 +0900

    feat: add Lambda execution role scoped to the Seasonings table

---
+++ terraform/iam.tf
@@ -0,0 +1,42 @@
+resource "aws_iam_role" "lambda_exec" {
+  name = "${local.name_prefix}-lambda-exec"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "lambda.amazonaws.com" }
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy" "lambda_dynamodb" {
+  name = "${local.name_prefix}-lambda-dynamodb"
+  role = aws_iam_role.lambda_exec.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:Query",
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:DeleteItem",
+        ]
+        Resource = aws_dynamodb_table.seasonings.arn
+      }
+    ]
+  })
+}
